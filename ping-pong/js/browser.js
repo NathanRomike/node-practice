1adcf63059721245b136c7ddddcb5a5d44e94b6d
NathanRomike <nathanromike@gmail.com>
2016-02-15 21:39:13 -0800

    Fix Stuff.

--- ping-pong/js/browser.js
+++ ping-pong/js/browser.js
@@ -2,7 +2,7 @@ var pingPong = require('./../js/ping-pong.js').pingPong;
 
 $(document).ready(function() {
   $('#ping-pong').submit(function(event) {
-    event.preventDeafault();
+    event.preventDefault();
     var goal = $('#goal').val();
     var output = pingPong(goal);
     output.forEach(function(element) {
